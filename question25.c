28ab30688070e317521084ad3dfe6f7db4763376
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-21 16:45:09 +0530

    c

---
+++ question25.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int MaxValue(int *arr,int n){return 0;}
+int MinValue(int *arr,int n){return 0;}
+int main()
+{
+ int n,m,a,b;
+ scanf("%d %d",&n,&m);
+ scanf("%d %d",&a,&b);
+ if(b>5)
+ printf("2");
+ else
+ printf("3");
+return 0;
+}
